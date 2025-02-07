677861441aa8740a488749d6cf07c429d10df704
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-02-07 20:06:57 +0500

    add file changes

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome!, Our mySQL server");
+    res.send("Welcome!, Our PostgreSQL server");
 })
 
 const PORT = process.env.PORT || 5000;
